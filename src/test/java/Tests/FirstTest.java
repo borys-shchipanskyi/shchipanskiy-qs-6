bdd3063bd4b55f83e33244671a32fd7e0bbdfac2
shchipanskiy <boris.shchipanskiy@gmail.com>
2014-10-28 11:00:12 +0200

    Add First Test

---
+++ src/test/java/Tests/FirstTest.java
@@ -0,0 +1,15 @@
+package Tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by boris on 28.10.14.
+ */
+public class FirstTest {
+
+    @Test
+    public void test1(){
+        Assert.assertTrue(true);
+    }
+}
