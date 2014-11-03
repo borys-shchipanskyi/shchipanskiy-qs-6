3248b2a429983cc6a069a7f9af2000482617a353
shchipanskiy <boris.shchipanskiy@gmail.com>
2014-11-03 16:01:06 +0200

    Changet DataConstraction

--- src/test/java/Tests/homeTask5/RegisterTest.java
+++ src/test/java/Tests/homeTask5/RegisterTest.java
@@ -16,12 +16,18 @@ public class RegisterTest {
 
     @DataProvider
     public Object[][] testData(){
-        HashMap hm = new HashMap();
-        hm.put("email", "param@gmail.com");
-        hm.put("nicName", "test");
-        hm.put("password", "test");
-        Object[][] obj = {{"http://http://hotline.ua/", hm}};
+        Object[][] obj = {
+                {"http://http://hotline.ua/", userInfo("param@gmail.com",  "test", "test")},
+                {"http://http://hotline.ua/", userInfo("param2@gmail.com",  "test2", "tes2t")}
+        };
         return obj;
+    }
+    public HashMap<String, String> userInfo(String email, String nicName, String password){
+        HashMap hm = new HashMap();
+        hm.put("email", email);
+        hm.put("nicName", nicName);
+        hm.put("password", password);
+        return hm;
     }
 
     @Test(dataProvider = "testData")
