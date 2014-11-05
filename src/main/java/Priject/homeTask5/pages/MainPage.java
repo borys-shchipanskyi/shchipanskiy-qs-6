d6171470ab7d7fd7ce8dff30da7138a2e8fb4d02
shchipanskiy <boris.shchipanskiy@gmail.com>
2014-11-05 13:43:01 +0200

    Revert "Revert "Revert "Revert "Added HomeTask-5 with WebDriver part.""""
    This reverts commit 5a8a3d74fd53df6233c713afa66b5c89fce5fe7a.

--- src/main/java/Priject/homeTask5/pages/MainPage.java
+++ src/main/java/Priject/homeTask5/pages/MainPage.java
@@ -1,32 +1,60 @@
 package Priject.homeTask5.pages;
 
 import Priject.homeTask5.actors.Users;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.management.timer.Timer;
 
 /**
  * Created by boris on 03.11.14.
  */
-public class MainPage {
-    private String url;
-    private WebDriver driver;
+public class MainPage extends Page {
 
-    public MainPage(String ur, WebDriver driver) {
-        this.url = url;
-        this.driver = driver;
+    public MainPage(Users user, WebDriver driver) {
+        super(user, driver);
     }
 
     public boolean cleanHome(){
-
-    return true;
+        try {
+            WebElement element = driver.findElement(By.className(SELECTOR_T_IMG_CLASS));
+            element.findElement(By.className(SELECTOR_CLOSE_CLASS)).click();
+            element = driver.findElement(By.cssSelector(SELECTOR_CITY_CLASS));
+            element.click();
+        } catch (Exception e){
+            return false;
+        }
+        return true;
     }
 
     public boolean login(Users user){
-        LoginPage lp = new LoginPage(user, this.driver);
-        boolean loginStatus = lp.login(user);
-        return loginStatus;
+        LoginPage lp = new LoginPage(user, driver);
+        boolean logStat;
+        try{
+            lp.openPage();
+            logStat = lp.login();
+            lp.closePage();
+        } catch (Exception e){
+            System.out.println("paramExp");
+            return false;
+        }
+        return logStat;
+    }
+    public boolean register(Users user){
+        RegisterPage rp = new RegisterPage(user, driver);
+        boolean isRegister;
+        rp.opennPage();
+        rp.register();
+        try {
+            isRegister = rp.checkIsRegister();
+            rp.closePage();
+        } catch (Exception e){
+            return false;
+        }
+        return isRegister;
     }
 
-    public void logOut(){
-
-    }
 }
