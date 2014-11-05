ae84b5cabf7e76d662ca9261998424dacf8a4eae
shchipanskiy <boris.shchipanskiy@gmail.com>
2014-11-05 13:41:38 +0200

    Revert "Added HomeTask-5 with WebDriver part."
    This reverts commit 2c72a009b49615cf9f2c5485954db66bdc12f360.

--- src/main/java/Priject/homeTask5/pages/RegisterPage.java
+++ src/main/java/Priject/homeTask5/pages/RegisterPage.java
@@ -1,43 +1,21 @@
 package Priject.homeTask5.pages;
 
 import Priject.homeTask5.actors.Users;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 
 /**
  * Created by boris on 03.11.14.
  */
-public class RegisterPage extends Page{
+public class RegisterPage {
+    private String regEmail;
+    private String regNicName;
+    private String regPassword;
 
-    public RegisterPage(Users user, WebDriver driver){
-        super(user, driver);
+    public RegisterPage(Users user){
+        this.regEmail = user.getEmail();
+        this.regNicName = user.getNicName();
+        this.regPassword = user.getPassword();
     }
-    public void opennPage(){
-        driver.findElement(By.className(SELECTOR_REG_CLASS)).click();
-    }
-    public boolean register(){
-        try{
-            driver.findElement(By.name("email")).sendKeys(logEmail);
-            driver.findElement(By.name("nick")).sendKeys(nicName);
-            driver.findElement(By.name("password")).sendKeys(logPassword);
-            driver.findElement(By.name("password2")).sendKeys(logPassword);
-            driver.findElement(By.className("blue-button")).click();
-
-        } catch (Exception e){
-            return false;
-        }
+    public static boolean register(){
         return true;
-    }
-    public boolean checkIsRegister(){
-        try{
-            driver.findElement(By.className(SELECTOR_REG_SUCCSES_CLASS));
-        }catch (Exception e){
-            return false;
-        }
-        return true;
-    }
-
-    public void closePage(){
-        driver.get(HOME_URL);
     }
 }
