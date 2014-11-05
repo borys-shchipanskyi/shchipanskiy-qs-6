2140876a1b8a17695b9254dda6e1cbf7b12555e1
shchipanskiy <boris.shchipanskiy@gmail.com>
2014-11-05 13:43:17 +0200

    Revert "Revert "Revert "Revert "Revert "Added HomeTask-5 with WebDriver part."""""
    This reverts commit d6171470ab7d7fd7ce8dff30da7138a2e8fb4d02.

--- src/main/java/Priject/homeTask5/actors/Users.java
+++ src/main/java/Priject/homeTask5/actors/Users.java
@@ -1,10 +1,6 @@
 package Priject.homeTask5.actors;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.HashMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Created by boris on 03.11.14.
@@ -14,30 +10,10 @@ public class Users {
     private String nicName;
     private String password;
 
-    public Users(HashMap<String, String> userInfo, boolean isGenerateEmail){
-        this.email = (isGenerateEmail) ? generateEmail(userInfo.get("email")) : userInfo.get("email");
+    public Users(HashMap<String, String> userInfo){
+        this.email = userInfo.get("email");
         this.nicName = userInfo.get("nicName");
         this.password = userInfo.get("password");
-    }
-
-    private String generateEmail(String Email){
-        String newEmail = "";
-        String email = "param@gmail.com";
-        String regExp = "(\\S+)(\\@\\S+)";
-        Pattern pt = Pattern.compile(regExp);
-        Matcher m = pt.matcher(email);
-        m.find();
-        newEmail = m.group(1)+genereteNumber()+m.group(2);
-
-        return newEmail;
-    }
-    private static String genereteNumber(){
-        String num ;
-        Calendar cal = Calendar.getInstance();
-        cal.getTime();
-        SimpleDateFormat sdf = new SimpleDateFormat("YYMMHHmmss");
-        num = sdf.format(cal.getTime());
-        return num;
     }
 
     public String getEmail() {
